0d4c2c125c342b07f979c346bb5a861c99ba8a0b
youda <yudahyeon2@naver.com>
2023-02-19 16:44:02 +0900

    Mel-Spectrogram method add

--- test.py
+++ test.py
@@ -4,20 +4,28 @@ import librosa.display
 import numpy as np
 import matplotlib.cm as cm
 
-FIG_SIZE = (15,10)
-n_fft=4096
-win_length = 4096
-hop_length=1024
-n_mels = 256
+frame_length = 0.125 # window length
+frame_stride = 0.010 # frame_stride 1ms단위로 뽑음
+''' 한 칸은 0.015초(15ms) 겹침'''
+
+def Mel_Spectrogram(wav_file):
+    '''Mel Spectogram을 구하는 함수'''
+    y,sr = librosa.load(wav_file)
+    input_nftt = int(round(sr*frame_length))
+    input_stride = int(round(sr*frame_stride))
+
+    S = librosa.feature.melspectrogram(y=y, n_mels=40, n_fft=input_nftt, hop_length=input_stride)
+
+    print("Wav Lenght:{} , Mel_Spectrogram:{}".format(len(y)/sr , np.shape(S)))
+    
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', sr=sr, hop_length=input_stride, x_axis='time')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Mel-Spectrogram')
+    plt.tight_layout()
+    plt.savefig('Mel-Spectrogram example.png')
+    plt.show()
+    return S
 
 audio_path = "C:/Users/ICT/Desktop/IoT/AI/E02/S000001/000000.wav"
-y, sr = librosa.load(audio_path)
-''' y : 파형의 amplitude값
-    sr : sampling rate'''
-print('amplitude value:' , y)
-
-D = np.abs(librosa.stft(y, n_fft=n_fft, win_length = win_length, hop_length=hop_length))
-mel_spec = librosa.feature.melspectrogram(S=D, sr=sr, n_mels=n_mels, hop_length=hop_length, win_length=win_length)
-librosa.display.specshow(librosa.amplitude_to_db(mel_spec, ref=0.00002), sr=sr, hop_length = hop_length, y_axis='mel', x_axis='time', cmap = cm.jet)
-plt.colorbar(format='%2.0f dB')
-plt.show()
+mel_spec = Mel_Spectrogram(audio_path)
