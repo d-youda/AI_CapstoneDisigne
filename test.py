655a09e47836eb29bca4feb1f3df0b02149111d8
youda <yudahyeon2@naver.com>
2023-02-19 12:27:18 +0900

    스펙트럼으로 나타낸 음성 데이터

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import librosa
 import matplotlib.pyplot as plt
 import librosa.display
+import numpy as np
+import matplotlib.cm as cm
 
 FIG_SIZE = (15,10)
 audio_path = "C:/Users/ICT/Desktop/IoT/AI/E02/S000001/000000.wav"
@@ -9,9 +11,9 @@ y, sr = librosa.load(audio_path)
     sr : sampling rate'''
 print('amplitude value:' , y)
 
-plt.figure(figsize=FIG_SIZE)
-#librosa.display.wavplot(y,sr,alpha=0.4)
-librosa.display.waveshow(y,sr,alpha=0.4)
-plt.ylabel("Amplitude")
-plt.title("Waveform")
+stft_result = librosa.stft(y, n_fft=4096, win_length = 4096, hop_length=1024)
+D = np.abs(stft_result)
+S_dB = librosa.power_to_db(D, ref=np.max)
+librosa.display.specshow(S_dB, sr=sr, hop_length = 1024, y_axis='linear', x_axis='time', cmap = cm.jet)
+plt.colorbar(format='%2.0f dB')
 plt.show()
