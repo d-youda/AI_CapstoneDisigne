6b274204ac2023376f1ace164e2d9226f44bb5a1
youda <yudahyeon2@naver.com>
2023-02-24 14:51:00 +0900

    add MFCC and save spectogram to .jpg file

--- test.py
+++ test.py
@@ -7,25 +7,29 @@ import matplotlib.cm as cm
 frame_length = 0.125 # window length
 frame_stride = 0.010 # frame_stride 1ms단위로 뽑음
 ''' 한 칸은 0.015초(15ms) 겹침'''
+def STFT(wav_file):
+    y, sr = librosa.load(wav_file)
+
+    stft_result = librosa.stft(y , n_fft=2048, win_length = 2048, hop_length=1024)
+    return np.abs(stft_result)
 
 def Mel_Spectrogram(wav_file):
     '''Mel Spectogram을 구하는 함수'''
-    y,sr = librosa.load(wav_file)
+    y, sr = librosa.load(wav_file)
     input_nftt = int(round(sr*frame_length))
     input_stride = int(round(sr*frame_stride))
 
-    S = librosa.feature.melspectrogram(y=y, n_mels=40, n_fft=input_nftt, hop_length=input_stride)
-
-    print("Wav Lenght:{} , Mel_Spectrogram:{}".format(len(y)/sr , np.shape(S)))
+    D = STFT(wav_file)
+    S = librosa.feature.melspectrogram(S=D, n_mels=40, n_fft=input_nftt, hop_length=input_stride)
+    mfcc = librosa.feature.mfcc(S=librosa.power_to_db(S), sr=sr, n_mfcc=20)
     
-    plt.figure(figsize=(10, 4))
-    librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', sr=sr, hop_length=input_stride, x_axis='time')
-    plt.colorbar(format='%+2.0f dB')
+    plt.figure(figsize=(10,4))
+    librosa.display.specshow(librosa.power_to_db(mfcc, ref=np.max),y_axis='mel', sr=sr, hop_length=input_stride, x_axis='time')
     plt.title('Mel-Spectrogram')
     plt.tight_layout()
-    plt.savefig('Mel-Spectrogram example.png')
-    plt.show()
-    return S
+    #plt.show()
 
-audio_path = "C:/Users/ICT/Desktop/IoT/AI/E02/S000001/000000.wav"
-mel_spec = Mel_Spectrogram(audio_path)
+for i in range(1,63):
+    audio_path = f"C:/Users/ICT/Desktop/youda/AI/emotion_data/emotion/pleasure/0029_G2A4E1S0C0_KJE/0029_G2A4E1S0C0_KJE_{str(i).zfill(6)}.wav"
+    mel_spec = Mel_Spectrogram(audio_path)
+    plt.savefig(f'Mel-Spectrogram example{int(str(i).zfill(6))}.png')
